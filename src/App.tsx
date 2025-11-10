d3162e0021dd3cc72b4ddfff8a253e9f86af8d00
Benzity <benzity@naver.com>
2025-11-10 19:09:17 +0900

    change: modify the route of components, pages folder (inside to src)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import '../pages/Login/styles/global.css'; // 전역 스타일 적용
-import FixedScreenLayout from '../components/layout/FixedScreenLayout/FixedScreenLayout';
-import Login from '../pages/Login/Login';
+import "./Login/styles/global.css"; // 전역 스타일 적용
+import FixedScreenLayout from "./components/layout/FixedScreenLayout/FixedScreenLayout";
+import Login from "./Login/Login";
 
 function App() {
   return (
