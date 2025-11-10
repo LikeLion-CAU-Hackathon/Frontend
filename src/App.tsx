b9a6a2074e87c7d31ec4b7f1cc31a6613bba61bb
Benzity <benzity@naver.com>
2025-11-10 23:14:58 +0900

    change: routes (App.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import CalendarPage from './pages/CalendarPage'
-import '../pages/Login/styles/global.css'; // 전역 스타일 적용
-import FixedScreenLayout from '../components/layout/FixedScreenLayout/FixedScreenLayout';
-import Login from '../pages/Login/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CalendarPage from './pages/CalendarPage';
+import './Login/styles/global.css'; // 전역 스타일 적용
+import FixedScreenLayout from './components/layout/FixedScreenLayout/FixedScreenLayout';
+import Login from './Login/Login';
 
 function App() {
   return (
     <FixedScreenLayout>
-      {/* 라우터가 있다면 여기에 <Router> 등을 설정 */}
       <Router>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/calendar" element={<CalendarPage />} />
-          </Routes>     
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+        </Routes>
       </Router>
-   </FixedScreenLayout>
-        
-  )
+    </FixedScreenLayout>
+  );
 }
 
 export default App;
