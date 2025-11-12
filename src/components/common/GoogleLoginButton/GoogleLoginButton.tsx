a0aead88002187e2ca6aeedaa55f66c11e58ed10
Benzity <benzity@naver.com>
2025-11-12 16:39:10 +0900

    feat: connect to backend auth login function (google)

--- src/components/common/GoogleLoginButton/GoogleLoginButton.tsx
+++ src/components/common/GoogleLoginButton/GoogleLoginButton.tsx
@@ -1,27 +1,20 @@
 import styles from './GoogleLoginButton.module.css';
 import googleLoginButtonImage from '../../../assets/images/google-login-button.svg';
 
-const LOCAL_GOOGLE_AUTH_URL = 'http://localhost:8080/oauth2/authorization/google';
-const PROD_GOOGLE_AUTH_URL = 'https://hackathon-santa.p-e.kr/oauth2/authorization/google';
-
-const getGoogleAuthUrl = () => {
-  const envUrl = import.meta.env?.VITE_GOOGLE_AUTH_URL;
-  if (envUrl) return envUrl;
-
-  if (typeof window === 'undefined') {
-    return LOCAL_GOOGLE_AUTH_URL;
-  }
-
-  const isLocalhost =
-    window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-
-  return isLocalhost ? LOCAL_GOOGLE_AUTH_URL : PROD_GOOGLE_AUTH_URL;
-};
+const GOOGLE_AUTH_BASE_URL =
+  import.meta.env?.VITE_GOOGLE_AUTH_URL ??
+  'https://hackathon-santa.p-e.kr/oauth2/authorization/google';
 
 const GoogleLoginButton = () => {
   const handleLogin = () => {
-    const googleAuthUrl = getGoogleAuthUrl();
-    window.location.assign(googleAuthUrl);
+    const currentOrigin = window.location.origin;
+    const callbackUrl = new URL('/login', currentOrigin);
+    callbackUrl.searchParams.set('redirect', '/calendar');
+
+    const authUrl = new URL(GOOGLE_AUTH_BASE_URL);
+    authUrl.searchParams.set('redirect_uri', callbackUrl.toString());
+
+    window.location.assign(authUrl.toString());
   };
 
   return (
