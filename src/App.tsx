081709b548da63364d94b1607d2a02b679cfadbe
Jihaeee <elinkang02@gmail.com>
2025-11-13 18:14:10 +0900

    feat: 답변 컴포넌트 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import CalendarPage from "./pages/calendar/CalendarPage";
 import "./pages/Login/styles/global.css"; // 전역 스타일 적용
 import FixedScreenLayout from "./components/layout/FixedScreenLayout/FixedScreenLayout";
 import Login from "./pages/Login/Login";
+import AnswerListPage from "./pages/answers/AnswerListPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/answer-list" element={<AnswerListPage />} />
         </Routes>
       </Router>
     </FixedScreenLayout>
