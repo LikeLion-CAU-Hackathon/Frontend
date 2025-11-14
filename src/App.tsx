caa2c4d8795c7dc5783a11e84bb5d74d28aacda8
Benzity <benzity@naver.com>
2025-11-15 01:18:26 +0900

    feat: initial comment pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./pages/Login/styles/global.css"; // 전역 스타일 적용
 import FixedScreenLayout from "./components/layout/FixedScreenLayout/FixedScreenLayout";
 import Login from "./pages/Login/Login";
 import Answer from "./pages/Answer/Answer";
+import Comments from "./pages/Comments/Comments";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/calendar" element={<CalendarPage />} />
           <Route path="/answer" element={<Answer />} />
+          <Route path="/comments" element={<Comments />} />
         </Routes>
       </Router>
     </FixedScreenLayout>
