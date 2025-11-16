1c62c3312a724044e7f19c98329f6e3b0ce4d35e
Jihaeee <elinkang02@gmail.com>
2025-11-17 00:49:50 +0900

    feat: 날짜와 답변 여부에 따른 다른 스탬프 이미지 렌더링

--- src/components/calendar/CalendarCard.tsx
+++ src/components/calendar/CalendarCard.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import stampImage from '../../assets/images/stamp.svg'
 import type { Card } from '../../types/card'
 import { stamps } from '../../utils/stampLoader';
+import { isCardAfterToday, isCardBeforeToday, isCardOpenableToday } from '../../utils/date';
+import expiredStamp from '../../assets/images/stamp/expiredStamp.png'
 
 interface CalendarCardProps {
     card: Card;
@@ -10,20 +12,43 @@ interface CalendarCardProps {
 }
 
 const CalendarCard = ({ card, onClick }: CalendarCardProps) => {
-
+  // 기본 
   const answeredStamp = stamps[ (card.id - 1) % stamps.length];
-  // console.log("stamps length:", stamps.length);
-  // console.log("스탬프 이미지 확인:", answeredStamp);
 
+  let stampToShow: string | null = null;
+  let opacity = 1;
+
+  // TODO1: 오늘인데 답변 안했으면 opacity=0.9로
+  if (isCardOpenableToday(card.id) && !card.isAnswered) {
+    stampToShow = answeredStamp;
+    opacity = 0.6
+
+  } else if (isCardOpenableToday(card.id) && card.isAnswered){
+    stampToShow = answeredStamp;
+
+  // TODO2: 오늘 이전인데 답변 안했으면 "expired" 표시
+  }   else if (isCardBeforeToday(card.id) && !card.isAnswered) {
+    stampToShow = expiredStamp;
+
+  // TODO3: 오늘 이전인데 답변 했으면 원래처럼 우표 표시 
+  } else if (isCardBeforeToday(card.id) && card.isAnswered) {
+    stampToShow = answeredStamp;
+  }  else if (isCardAfterToday(card.id)) {
+    stampToShow = null;
+  }
+ 
   return (
     // 나중에 isOpened가 true일 때 편지지 열리게 추가 
     <CardSection onClick={() => onClick(card.id)}> 
         <CardImage src={stampImage} alt='stamp background' /> 
-
-        { /* 답변이 완료된 경우(테스트용) : ! 지우기 */}
-        {!card.isAnswered && (
-        <Stamp src={answeredStamp} alt="answered-stamp" />
-      )}
+        { stampToShow && (
+          <Stamp
+             src={stampToShow}
+             alt="stampImage"
+             style={{opacity}}
+            isExpired={stampToShow === expiredStamp}
+          />
+        )}
 
     </CardSection> 
   )
@@ -41,10 +66,16 @@ const CardImage = styled.img`
   cursor: pointer;
 `
 
-const Stamp = styled.img`
+const Stamp = styled.img<{ isExpired?: boolean }>`
   position: absolute;
   width: 60px;
   z-index:1;
   top:10px;
-  right:15px;
+  right:16px;
+
+  ${({ isExpired }) =>
+    isExpired &&
+    `
+    top: 45px;
+    `}
 `
