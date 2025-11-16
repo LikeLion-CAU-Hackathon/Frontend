a74ebf28fae25bba835d00e1d2e683d5eb3cfb49
Benzity <benzity@naver.com>
2025-11-17 02:53:30 +0900

    api: axios localstroage token받게

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from "../constants/baseURL";
+import { getAccessToken, clearToken } from "../utils/token";
 
 export const axiosAPI = axios.create({
   baseURL: BASE_URL,
@@ -7,7 +8,14 @@ export const axiosAPI = axios.create({
 });
 
 axiosAPI.interceptors.request.use(
-  (config) => config,
+  (config) => {
+    const token = getAccessToken();
+    if (token) {
+      config.headers = config.headers ?? {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
   (error) => Promise.reject(error)
 );
 
@@ -16,7 +24,7 @@ axiosAPI.interceptors.response.use(
   (error) => {
     if (error.response?.status === 401) {
       alert("로그인 세션이 만료되었습니다. 다시 로그인해주세요.");
-      window.location.href = "/";
+      clearToken();
     }
     return Promise.reject(error);
   }
