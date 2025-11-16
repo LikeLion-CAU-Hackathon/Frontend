27d3b67e3bd0ac29e54ce9eb98acb6324600db78
Jihaeee <elinkang02@gmail.com>
2025-11-16 18:07:26 +0900

    feat: 스탬프 날짜 기준 렌더링 다르게 하는 로직 추가

--- src/components/calendar/CalendarCard.tsx
+++ src/components/calendar/CalendarCard.tsx
@@ -2,6 +2,7 @@
 import styled from 'styled-components'
 import stampImage from '../../assets/images/stamp.svg'
 import type { Card } from '../../types/card'
+import { isCardAfterToday } from '../../utils/date'
 
 interface CalendarCardProps {
     card: Card;
@@ -10,11 +11,20 @@ interface CalendarCardProps {
 
 
 const CalendarCard = ({ card, onClick }: CalendarCardProps) => {
+  const isAfterToday = isCardAfterToday(card.id);
+  const showQuestionMark = isAfterToday;
+  const showExpired = card.isExpired;
 
   return (
     // 나중에 isOpened가 true일 때 편지지 열리게 추가 
     <CardSection onClick={() => onClick(card.id)}> 
         <CardImage src={stampImage} alt='stamp background' /> 
+        {showQuestionMark && (
+          <QuestionMark>?</QuestionMark>
+        )}
+        {showExpired && (
+          <ExpiredLabel>만료</ExpiredLabel>
+        )}
     </CardSection> 
   )
 }
@@ -23,8 +33,25 @@ export default CalendarCard
 
 const CardSection = styled.section`
     margin: 0;
+    position: relative;
 `
 
 const CardImage = styled.img`
   cursor: pointer;
+`
+
+const QuestionMark = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  color: black
+  pointer-events: none;
+`
+
+const ExpiredLabel = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  color: #ff0000;
+  padding: 4px 8px;
 `
