3cb59ab85d48dd4853d5ae58558a6454b3d562e7
Jihaeee <elinkang02@gmail.com>
2025-11-17 00:12:03 +0900

    feat: 우표 그림 추가

--- src/components/calendar/CalendarCard.tsx
+++ src/components/calendar/CalendarCard.tsx
@@ -2,19 +2,29 @@
 import styled from 'styled-components'
 import stampImage from '../../assets/images/stamp.svg'
 import type { Card } from '../../types/card'
+import { stamps } from '../../utils/stampLoader';
 
 interface CalendarCardProps {
     card: Card;
     onClick: (id: number ) => void;
 }
 
-
 const CalendarCard = ({ card, onClick }: CalendarCardProps) => {
+
+  const answeredStamp = stamps[ (card.id - 1) % stamps.length];
+  // console.log("stamps length:", stamps.length);
+  // console.log("스탬프 이미지 확인:", answeredStamp);
 
   return (
     // 나중에 isOpened가 true일 때 편지지 열리게 추가 
     <CardSection onClick={() => onClick(card.id)}> 
         <CardImage src={stampImage} alt='stamp background' /> 
+
+        { /* 답변이 완료된 경우(테스트용) : ! 지우기 */}
+        {!card.isAnswered && (
+        <Stamp src={answeredStamp} alt="answered-stamp" />
+      )}
+
     </CardSection> 
   )
 }
@@ -22,9 +32,19 @@ const CalendarCard = ({ card, onClick }: CalendarCardProps) => {
 export default CalendarCard
 
 const CardSection = styled.section`
+    position: relative;
     margin: 0;
+  
 `
 
 const CardImage = styled.img`
   cursor: pointer;
+`
+
+const Stamp = styled.img`
+  position: absolute;
+  width: 60px;
+  z-index:1;
+  top:10px;
+  right:15px;
 `
--- src/components/calendar/LetterContent.tsx
+++ src/components/calendar/LetterContent.tsx
@@ -115,4 +115,3 @@ const ButtonSection = styled.section`
 
 
 
-
