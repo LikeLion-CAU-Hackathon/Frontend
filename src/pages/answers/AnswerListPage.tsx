2e6f70cbefdec5ec6602e2d485bcfe10780ed711
Jihaeee <elinkang02@gmail.com>
2025-11-17 20:36:35 +0900

    feat: 답변모음페이지에서 뒤로가기 시 전에 상태와 무관하게 무조건 /calendar로 이동

--- src/pages/answers/AnswerListPage.tsx
+++ src/pages/answers/AnswerListPage.tsx
@@ -99,6 +99,21 @@ const AnswerListPage = () => {
   const [backgroundOffset, setBackgroundOffset] = useState(() => -currentSlide * 100);
   const rafRef = useRef<number | null>(null);
 
+  // 기본 뒤로가기 차단하고 뒤로가기 하면 무조건 캘린더로
+  useEffect(() => {
+    navigate(`/answer-list?questionId=${cardId}`, {replace: true});
+
+    window.history.pushState(null, "", window.location.href);
+    window.history.pushState(null, "", window.location.href);
+
+    const handleBack = () => {
+      navigate("/calendar", { replace : true });
+    };
+
+    window.addEventListener("popstate", handleBack);
+    return () => window.removeEventListener("popstate", handleBack);
+  }, []);
+
   // URL params에서 cardId 가져오기
   const [searchParams, setSearchParams] = useSearchParams();
   const cardId = searchParams.get("cardId") || searchParams.get("questionId");
@@ -206,19 +221,6 @@ const AnswerListPage = () => {
       setAnimationState(null);
     }, 650);
   }, [animationState, cardId, currentSlide, navigate, question]);
-
-  // 더미데이터 
-  // TODO: 답변 API 불러오기 
-  // const allAnswers: Answer[] = [
-  //   { id: 1, author: "잘생긴 루돌프", date: "DEC 7", time: "18:44", contents: "아ㅓ알ㅇ러알아러아러아아ㅓ아ㅓㅏ랄ㅇ라얼ㅇ러알알ㅇㄹ아알ㅇ라이라이랑랑라리ㅏㄹㅏㄹ아알아러아ㅓ아러ㅏ러아러ㅏㅓㅇㄹ아ㅓㄹㅇ러ㅓㄹ러ㅏㅓㅇ라러ㅏㅓ라ㅓ러라러ㅏ러아ㅓ라러라ㅓ러ㅏㅓㅏ어라얼아러아렁렁라ㅏㅓ알댜ㅏ러야랑ㄹ아러아러아렁어ㅏㅓㄹ아ㅓ랑러앙ㄹ어러아라ㅓ러ㅏ어아ㅓㅏㅇㄹ알알라ㅏ알알", likes: 99, comments: 99 },
-  //   { id: 2, author: "예쁜 산타", date: "DEC 7", time: "16:24", contents: "2번", likes: 99, comments: 99 },
-  //   { id: 3, author: "건강한 개발자", date: "DEC 7", time: "12:28", contents: "3번", likes: 19, comments: 9 },
-  //   { id: 4, author: "무례한 눈사람", date: "DEC 7", time: "11:59", contents: "4번", likes: 2, comments: 5 },
-  //   { id: 5, author: "잘생긴 산타", date: "DEC 7", time: "13:00", contents: "5번", likes: 2, comments: 0 },
-  //   { id: 6, author: "건강한 눈사람", date: "DEC 7", time: "14:30", contents: "6번", likes: 4, comments: 1 },
-  //   { id: 7, author: "크리스마스", date: "DEC 7", time: "14:30", contents: "7번", likes: 3, comments: 1 },
-   
-  // ];
 
   const answerChunks = useMemo(() => {
     const chunkSize = 4;
