d85766e6d5d197af4c78576fe35f22096d1d1fc5
Jihaeee <elinkang02@gmail.com>
2025-12-15 15:47:33 +0900

    refactor: 파일명 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import CalendarPage from "./pages/calendar/CalendarPage";
 import "./pages/login/styles/global.css"; // 전역 스타일 적용
 import FixedScreenLayout from "./components/layout/fixedscreenlayout/FixedScreenLayout";
 import Login from "./pages/login/Login";
-import WriteAnswer from "./pages/writeanswer/WriteAnswer";
+import Answer from "./pages/answer/Answer";
 import Comments from "./pages/comments/Comments";
-import AnswerListPage from "./pages/answers/AnswerListPage";
+import AnswerListPage from "./pages/answer-list/AnswerListPage";
 import PrivateRoute from "./components/common/PrivateRoute";
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
 
           { /* 로그인이 안되면 접근 불가능 */}
           <Route element= {<PrivateRoute />} >
-            <Route path="/answer" element={<WriteAnswer />} />
+            <Route path="/answer" element={<Answer />} />
             <Route path="/comments" element={<Comments />} />
             <Route path="/answer-list" element={<AnswerListPage />} />
           </Route>
